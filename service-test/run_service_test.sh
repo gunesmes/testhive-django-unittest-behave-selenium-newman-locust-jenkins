9bef6bcfc6ac6978ef5a0f8755b82fba65fd473c
Mesut Güneş <gunesmes@gmail.com>
2019-02-27 13:12:19 +0300

    testhive update

--- service-test/run_service_test.sh
+++ service-test/run_service_test.sh
@@ -16,4 +16,4 @@ rm newman/*.html 2>/dev/null
 
 # run postman collection in newman with docker image
 docker pull gunesmes/newman-postman-html-report
-docker run --network host -v $PWD:/newman gunesmes/newman-postman-html-report run https://www.getpostman.com/collections/d3bbf2eb6acea1ad272f -e blog-local.postman_environment.json --reporters cli,html --reporter-html-template report-template.hbs
+docker run --network host -v $PWD:/newman gunesmes/newman-postman-html-report run blog-sample-service.postman_collection.json -e blog-local.postman_environment.json --reporters cli,html --reporter-html-template report-template.hbs

ecf5cfcc7ea23431853109d7585e55160a0c2f55
Mesut Güneş <gunesmes@gmail.com>
2019-11-17 00:59:51 +0300

    xml runner added

--- service-test/run_service_test.sh
+++ service-test/run_service_test.sh
@@ -8,6 +8,9 @@ echo -e "\n\n * * * * * * *  SERVICE TEST STARTED  * * * * * * *\n"
 # remove old reports
 rm newman/*.html 2>/dev/null
 
+# get custom docker image
+docker pull gunesmes/newman-postman-html-report
+
 # run postman collection in newman with local installation and collection json
 #newman run blog-sample-service.postman_collection.json -e blog-local.postman_environment.json --reporters cli,html --reporter-html-template report-template.hbs
 
@@ -15,5 +18,8 @@ rm newman/*.html 2>/dev/null
 #newman run https://www.getpostman.com/collections/ac3d0d9bbd8ae1bcfe5d -e blog-local.postman_environment.json --reporters cli,html --reporter-html-template report-template.hbs
 
 # run postman collection in newman with docker image
-docker pull gunesmes/newman-postman-html-report
-docker run --network host -v $PWD:/newman gunesmes/newman-postman-html-report run blog-sample-service.postman_collection.json -e blog-local.postman_environment.json --reporters cli,html --reporter-html-template report-template.hbs
+## html result
+# docker run --network host -v $PWD:/newman gunesmes/newman-postman-html-report run blog-sample-service.postman_collection.json -e blog-local.postman_environment.json --reporters cli,html --reporter-html-template report-template.hbs
+
+## xml result
+docker run --network host -v $PWD:/newman gunesmes/newman-postman-html-report run blog-sample-service.postman_collection.json -e blog-local.postman_environment.json --reporters junit --reporter-junit-export newman/service-test-report.xml

cac68f75fc40e0aeecefbe4b0d5b3f2e78e16ca3
Mesut Gunes <gunesmes@gmail.com>
2022-08-25 10:53:35 +0300

    premium paramater added to tests

--- service-test/restore_database.sql
+++ service-test/restore_database.sql
@@ -7,8 +7,8 @@ ALTER SEQUENCE src_users_id_seq RESTART WITH 1
 
 
 # ADD TWO TEST USER TO src_users
-INSERT INTO src_users ("username","email","address","birthday") VALUES('testusername1','test1@test.com','Turkey İstanbul Bakırköy 344252','2001-09-03');
-INSERT INTO src_users ("username","email","address","birthday") VALUES('testusername2','test2@test.com','Studio 103 Wellfield Road Roath Cardiff','2000-09-03');
+INSERT INTO src_users ("username","email","address","birthday", "premium") VALUES('testusername1','test1@test.com','Turkey İstanbul Bakırköy 344252','2001-09-03', false);
+INSERT INTO src_users ("username","email","address","birthday", "premium") VALUES('testusername2','test2@test.com','Studio 103 Wellfield Road Roath Cardiff','2000-09-03', false);
 
 
 # * * * * * *   RESTORE DATA FINISHED  * * * * * *
